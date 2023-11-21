eac3b06bc943d67e394806b7b1423e951552b56a
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2023-11-21 18:19:07 +0100

    Module 18: Html and css school

---
+++ Html-CSS/js/main.js
@@ -0,0 +1,8 @@
+// Collapisble
+
+var collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
